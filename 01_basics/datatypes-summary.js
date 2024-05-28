59f4f0601599077770d43088a2ab2634ef07b3a9
rao-shreya <122074645+rao-shreya@users.noreply.github.com>
2024-05-28 18:42:23 +0000

    Data type Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,54 @@
+// types of  Datatypes in js
+// 1- Primitive 
+// this is of 7 types : String, Number , null , undefined , Symbol , BigInt;
+
+
+const score = 100;
+const scoreValue = 100.3;
+
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+//uses of symbol
+
+const  id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId)
+
+
+// use of BigInt;
+
+// const bigNumber = 93408508508358058805805n;
+
+
+
+
+
+
+
+// 2- Reference (Non primitive)
+
+// Array , Object , Functions
+
+const heros = ["Shreya" , "Batman" , "Narutoo"]
+ let myObj = {
+   name: "Shreya" ,
+   age : 20 ,
+}
+
+const myFunction = function(){
+    console.log("Hello My Name is Shreya");
+}
+
+console.log(typeof 
+     bigNumber);
+
+console.log(typeof 
+        outsideTemp);
+console.log(typeof 
+            myFunction);        
+console.log(typeof 
+                anotherId);
