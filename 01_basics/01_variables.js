7454d81922914585331fbd2494486af77537cb48
rao-shreya <122074645+rao-shreya@users.noreply.github.com>
2024-05-28 11:28:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144533
+let accountEmail="shreya@google.com"
+var accountpassword ="12345"
+accountCity="Jaipur"
+let accountState
+// accountId = 2//not allowed
+// we now don't use var in js // problem of scope
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail="hc@gc.com"
+accountpassword="61725735"
+accountCity="Gorakhpur"
+
+console.log(accountId)
+console.table([accountId , accountEmail,accountpassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as never version 
+
+// alert(3+3) // we are usaing nodejs, not browser
+console.log(3+3);
+console.log("Shreya");
+
+//Code readability should be high
+
+
+// documentation - mdn by mozila organisation
+//ecma Script - for standards in js
+//tc39.es/ecma262/#sec-intro
+
+let name="shreya";  // string
+let age = 18;   // number
+let isLoggedIn = false;   //boolean
+
+//number => 2 to power 53
+// bigingt for too large number
+//string => " "
+// boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefine);
